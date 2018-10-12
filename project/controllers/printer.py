9cdb83a4740abcdb3793fec7c6289d8bdae5f02f
LiuZichen <danteliuzichen@163.com>
2018-10-12 16:10:25 +0800

    框架

---
+++ project/controllers/printer.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+import json
+from project import app
+from bottle import template, request
+
+
+@app.route('/', method='GET')
+def index():
+    return template('printer/index', message='')
+
+
+@app.route('/print', method=['GET', 'POST'])
+def printer():
+    if request.method == 'POST':
+        message =request.forms.get('text')
+        message =message+'22'
+        return json.dumps(message)
+        # return template('printer/index', message=message)
+    return template('printer/print', message='')
