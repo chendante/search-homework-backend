68de6d130452db18aec6d6c6161511abcfa09fe3
unknown <danteliuzichen@163.com>
2018-10-15 15:45:01 +0800

    部署

--- runserver.py
+++ runserver.py
@@ -7,5 +7,5 @@ from bottle import debug, run
 
 # debug(True)
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 8080))
     run(app, reloader=True, host='0.0.0.0', port=port)
